8a02dab543864b0c399949f9e6588aa898bdc25f
Tagir1612 <tagir_magadiev@mail.ru>
2024-07-21 14:10:55 +0500

    commit

--- module_3_2.py
+++ module_3_2.py
@@ -1,5 +1,6 @@
-def send_email(message,recipient,sender="university.help@gmail.com"):
-    if ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    if (("@" or (".com" or ".ru" or ".net")) not in (recipient and sender)
+            or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender)):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
     elif sender == recipient:
         print("Нельзя отправить письмо самому себе!")
@@ -10,6 +11,9 @@ def send_email(message,recipient,sender="university.help@gmail.com"):
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru',
+           sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru',
+           sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru',
+           sender='urban.teacher@mail.ru')
